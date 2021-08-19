de69a82e585651b42b9f46035ffa340f5b12f63a
Eric Kariuki <kariuki931@gmail.com>
2021-08-19 13:47:43 +0300

    Update ZombieFactory.sol

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -2,23 +2,33 @@ pragma solidity ^0.4.25;
 
 contract ZombieFactory {
 
+    //Create an event that blockchain clients can listen to
     event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
+    //Zombie blueprint
     struct Zombie {
         string name;
         uint dna;
     }
 
+    //Make an array of zombies
     Zombie[] public zombies;
 
+    //map zombies to their owners
     mapping (uint => address) public zombieToOwner;
+    
+    //map zombie owners to the amount of zombies they own
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string _name, uint _dna) internal {
+    
+        //create a zombie and add them to the zombies array
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        
+        //map the zombies id to their owner
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
